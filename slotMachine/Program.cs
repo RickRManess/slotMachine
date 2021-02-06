8e4cbb54e2c887a538a783c05a856c44c3e62231
Richard Maness <rickrmaness@gmail.com>
2021-02-06 09:13:24 -0500

    first implementation of grid display

--- slotMachine/Program.cs
+++ slotMachine/Program.cs
@@ -9,24 +9,36 @@ namespace slotMachine
             var rand = new Random();
 
 
-
-
             //2D array for slot machine display
-            int [,] slotMachine;
-            slotMachine = new int[3,3];
-              
+            int[,] slotMachine = new int[3, 3];
+            slotMachine.GetLength(0);
+            slotMachine.GetLength(1);
+
 
             //Nested loops to fill/print 2D array for slot machine display
-                for (int i = 0; i <= 2; i++)
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
                 {
-                   for (int j = 0; j <= 2; j++)
-            {
-                    slotMachine[i, j] = rand.Next(2);
-                    Console.Write(slotMachine[i, j]);
-                }
-            }
+                    slotMachine[i, j] = rand.Next(3);
+                    Console.Write(slotMachine[i, j] + " ");
+                } Console.WriteLine();
 
-          
+
+                // Check Middle Row for Winning Numbers
+                for (int x = 0; x < slotMachine.GetLength(0); x++)
+                    for (int y = 0; y < slotMachine.GetLength(1); y++)
+                    {
+                        if (slotMachine[x, 1] == 2)
+                        {
+                           
+                        };
+                    };
+            
+
+
+
+
+
 
 
 
@@ -36,6 +48,7 @@ namespace slotMachine
         }
     }
 }
+
 
 
             
