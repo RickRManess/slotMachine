b93f9af0e2c8862d0b62b19d5cb83a1502bbeb37
Richard Maness <rickrmaness@gmail.com>
2021-02-06 10:03:21 -0500

    fixed newline for grid display

--- slotMachine/Program.cs
+++ slotMachine/Program.cs
@@ -11,29 +11,37 @@ namespace slotMachine
 
             //2D array for slot machine display
             int[,] slotMachine = new int[3, 3];
-            slotMachine.GetLength(0);
-            slotMachine.GetLength(1);
+
+
 
 
             //Nested loops to fill/print 2D array for slot machine display
-            for (int i = 0; i < slotMachine.GetLength(0); i++)
-                for (int j = 0; j < slotMachine.GetLength(1); j++)
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
                 {
                     slotMachine[i, j] = rand.Next(3);
                     Console.Write(slotMachine[i, j] + " ");
-                } Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
 
 
-                // Check Middle Row for Winning Numbers
-                for (int x = 0; x < slotMachine.GetLength(0); x++)
-                    for (int y = 0; y < slotMachine.GetLength(1); y++)
+
+
+            // Check Middle Row for Winning Numbers => if alle numbers are the same
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (slotMachine[x, 1] == 2)  
                     {
-                        if (slotMachine[x, 1] == 2)
-                        {
-                           //add comment
-                        };
+                        //add comment
                     };
-            
+                };
+            }
+
 
 
 
@@ -51,7 +59,6 @@ namespace slotMachine
 
 
 
-            
 
 
 
@@ -60,5 +67,6 @@ namespace slotMachine
 
 
 
-        
-    
+
+
+
