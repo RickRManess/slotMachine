409962583603d5b4a07c0f886a762e7df7842c5f
Richard Maness <rickrmaness@gmail.com>
2021-02-09 18:08:42 -0500

    reformatted code to hopefully really put the functions where they belong and added coin display box

--- slotMachine/Program.cs
+++ slotMachine/Program.cs
@@ -10,6 +10,13 @@ namespace slotMachine
             var rand = new Random();
             int[,] slotMachine = new int[3, 3]; //2D array for slot machine display
 
+            //Coins Display 
+            int coins = 20;
+            Console.WriteLine("Press Enter to Spin Again");
+            Console.WriteLine("Bank = " + coins);
+            Console.WriteLine();
+
+
             //Nested loops to fill/print 2D array for slot machine display
             for (int i = 0; i < 3; i++)
             {
@@ -21,17 +28,49 @@ namespace slotMachine
                 Console.WriteLine();
             }
 
-            // Function to check for winning Grid 
-            static bool IsWinnigGrid(int[,] slotMachine)
+            // if-else to check for winning grid and display win/loss message 
+            if (IsWinnigGrid(slotMachine))
             {
-                if (slotMachine[1, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[1, 2])
+                Console.WriteLine("You Win =)");
+                spinSlotMachine();
+            }
+            else
+            {
+                Console.WriteLine("You Lose =(");
+                spinSlotMachine();
+            }
+
+        }
+        // Function to check for winning Grid 
+        static bool IsWinnigGrid(int[,] slotMachine)
+        {
+
+            if (slotMachine[1, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[1, 2])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }   // Function to initialize game 
+        static void spinSlotMachine()
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            Console.Clear();
+            var rand = new Random();
+            int[,] slotMachine = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
                 {
-                    return true;
+                    slotMachine[i, j] = rand.Next(3);
+                    Console.Write(slotMachine[i, j] + " ");
                 }
-                else
-                {
-                    return false;
-                }
+                Console.WriteLine();
             }
             if (IsWinnigGrid(slotMachine))
             {
@@ -42,35 +81,8 @@ namespace slotMachine
             {
                 Console.WriteLine("You Lose =(");
                 spinSlotMachine();
-                            }
-            // Function to initialize game 
-            static void spinSlotMachine()
-            {
-                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                Console.Clear();
-                var rand = new Random();
-                int[,] slotMachine = new int[3, 3];
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        slotMachine[i, j] = rand.Next(3);
-                        Console.Write(slotMachine[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                if (IsWinnigGrid(slotMachine))
-                {
-                    Console.WriteLine("You Win =)");
-                    spinSlotMachine();
-                }
-                else
-                {
-                    Console.WriteLine("You Lose =(");
-                    spinSlotMachine();
-                }
-
             }
+
         }
     }
 }
