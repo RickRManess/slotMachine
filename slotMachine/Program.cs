295213841ebd12d4df1dfc014e3779f9da88e1d6
Richard Maness <rickrmaness@gmail.com>
2021-02-11 19:13:10 -0500

    reduced duplicate code for displayCoinBank function

--- slotMachine/Program.cs
+++ slotMachine/Program.cs
@@ -8,69 +8,63 @@ namespace slotMachine
         {
 
             //Coins Display 
-            int coins = 20;
-            Console.WriteLine("Press Any Key to Spin ");
-            Console.WriteLine("Bank = " + coins);
-            Console.WriteLine();
-
-            //Function to spin slot machine and check for winning grid
-            spinSlotMachine();
-        }
-        // Function to genereate and display grid, check for winning grid, and display win/lose message 
-        static void spinSlotMachine()
-        {
-            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            static void displayCoinBank()
             {
-                Console.Clear();
-                displayCoinBank();
-                var rand = new Random();
-                int[,] slotMachine = new int[3, 3];
-                for (int i = 0; i < 3; i++)
+                int coins = 20;
+                Console.WriteLine("Press Any Key to Spin ");
+                Console.WriteLine("Bank = " + coins);
+                Console.WriteLine();
+            }
+            //Function to spin slot machine and check for winning grid
+            displayCoinBank();
+            spinSlotMachine();
+
+            // Function to genereate and display grid, check for winning grid, and display win/lose message 
+            static void spinSlotMachine()
+            {
+
+                while (Console.ReadKey().Key != ConsoleKey.Enter)
                 {
-                    for (int j = 0; j < 3; j++)
+                    Console.Clear();
+                    displayCoinBank();
+                    var rand = new Random();
+                    int[,] slotMachine = new int[3, 3];
+                    for (int i = 0; i < 3; i++)
                     {
-                        slotMachine[i, j] = rand.Next(3);
-                        Console.Write(slotMachine[i, j] + " ");
+                        for (int j = 0; j < 3; j++)
+                        {
+                            slotMachine[i, j] = rand.Next(3);
+                            Console.Write(slotMachine[i, j] + " ");
+                        }
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
-                }
-                static bool IsWinnigGrid(int[,] slotMachine)
-                {
-                    if (slotMachine[1, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[1, 2])
+                    static bool IsWinnigGrid(int[,] slotMachine)
                     {
-                        return true;
+                        if (slotMachine[1, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[1, 2])
+                        {
+                            return true;
+
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    if (IsWinnigGrid(slotMachine))
+                    {
+                        Console.WriteLine("You Win =)");
+                        spinSlotMachine();
+
                     }
                     else
                     {
-                        return false;
+                        Console.WriteLine("You Lose =(");
+                        spinSlotMachine();
                     }
                 }
-                if (IsWinnigGrid(slotMachine))
-                {
-                    Console.WriteLine("You Win =)");
-                    spinSlotMachine();
-                }
-                else
-                {
-                    Console.WriteLine("You Lose =(");
-                    spinSlotMachine();
-                }
             }
+
         }
-        // Function for Coin Bank Display 
-        static void displayCoinBank()
-        {
-            int coins = 20;
-            Console.WriteLine("Press Any Key to Spin Again");
-            Console.WriteLine("Bank = " + coins);
-            Console.WriteLine();
-        }
-
-
-
-
-
-
 
     }
 }
