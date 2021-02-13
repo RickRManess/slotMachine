3b143f5227f16ddf392b1eb52265e7ee057d9520
Richard Maness <rickrmaness@gmail.com>
2021-02-13 13:10:30 -0500

    Cleaned up spacing, changed the functions at the start

--- slotMachine/Program.cs
+++ slotMachine/Program.cs
@@ -6,67 +6,77 @@ namespace slotMachine
     {
         static void Main(string[] args)
         {
+            
+        }
+        /// <>
+        /// 
+        /// </This function displays the coin bank>
+        static void displayCoinBank()
+        {
+            int coins = 20;
+            Console.WriteLine("Press Any Key to Spin ");
+            Console.WriteLine("Bank = " + coins);
+            Console.WriteLine();
+        }
 
-            //Coins Display 
-            static void displayCoinBank()
+        //Functions to display coin bank spin slot machine and check for winning grid
+        //displayCoinBank();
+        //spinSlotMachine();
+
+       
+        static void spinSlotMachine()
+        {
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
             {
-                int coins = 20;
-                Console.WriteLine("Press Any Key to Spin ");
-                Console.WriteLine("Bank = " + coins);
-                Console.WriteLine();
-            }
-            //Functions to display coin bank; spin slot machine and check for winning grid
-            displayCoinBank();
-            spinSlotMachine();
-
-            // Function to genereate and display grid, check for winning grid, and display win/lose message 
-            static void spinSlotMachine()
-            {
-
-                while (Console.ReadKey().Key != ConsoleKey.Enter)
+                Console.Clear();
+                displayCoinBank();
+                var rand = new Random();
+                int[,] slotMachine = new int[3, 3];
+                for (int i = 0; i < 3; i++)
                 {
-                    Console.Clear();
-                    displayCoinBank();
-                    var rand = new Random();
-                    int[,] slotMachine = new int[3, 3];
-                    for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                     {
-                        for (int j = 0; j < 3; j++)
-                        {
-                            slotMachine[i, j] = rand.Next(3);
-                            Console.Write(slotMachine[i, j] + " ");
-                        }
-                        Console.WriteLine();
+                        slotMachine[i, j] = rand.Next(3);
+                        Console.Write(slotMachine[i, j] + " ");
                     }
-                    static bool IsWinnigGrid(int[,] slotMachine)
-                    {
-                        if (slotMachine[1, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[1, 2])
-                        {
-                            return true;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
-                    if (IsWinnigGrid(slotMachine))
-                    {
-                        Console.WriteLine("You Win =)");
-                        spinSlotMachine();
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("You Lose =(");
-                        spinSlotMachine();
-                    }
+                    Console.WriteLine();
                 }
+
+            }
+
+
+        }/// <Checks grid for win/loss>
+        /// 
+        /// </checks the middle line of the grid and displays you win/you lose text>
+        /// <param name="slotMachine"></param>
+        /// <You win></You lose>
+        static bool IsWinnigGrid(int[,] slotMachine)
+        {
+            if (slotMachine[1, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[1, 2])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            if (IsWinnigGrid(slotMachine))
+            {
+                Console.WriteLine("You Win =)");
+
+            }
+            else
+            {
+                Console.WriteLine("You Lose =(");
+
             }
 
         }
-
     }
+
 }
+
 
 
 
